a2b4ee38579d1823ed0eca57f9db31a0e6f6cfb2
sione-tenifa <johntenifa@yahoo.com>
2021-03-24 12:00:43 -0600

    functioning

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 from flask import Flask, request, render_template, jsonify, session
 from forex_python.converter import CurrencyRates
+import json
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "forex"
@@ -17,7 +19,12 @@ def index():
 @app.route('/converter', methods=['POST'])
 def curr_converter():
     
-
+    c = CurrencyRates()
     curr_one = request.form.get('cur1')
+    curr_two = request.form.get('cur2')
+    amount = request.form.get('value')
+    print(curr_one)
+    converted = c.convert(curr_one.upper(), curr_two.upper(), float(amount)) 
+    print(converted)
 
-    return render_template("converter.html", curr_one=curr_one)
+    return render_template("converter.html", curr_one=curr_one, curr_two=curr_two, converted=converted)
