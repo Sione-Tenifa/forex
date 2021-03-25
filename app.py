9ca5ab5e61e59556985ecb6d32ee8c859b38a3df
sione-tenifa <johntenifa@yahoo.com>
2021-03-25 08:31:44 -0600

    error message

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, render_template, jsonify, session
+from flask import Flask, request, render_template, jsonify, session, flash, redirect
 from forex_python.converter import CurrencyRates
 import json
 
@@ -13,18 +13,27 @@ app.debug = True
 def index():
 
     c = CurrencyRates()
-    print(c.get_rates('USD'))
+    print(c.get_rate)
     return render_template("index.html")
 
 @app.route('/converter', methods=['POST'])
 def curr_converter():
     
     c = CurrencyRates()
+    Currency_list = c.get_rates('USD')
+
     curr_one = request.form.get('cur1')
     curr_two = request.form.get('cur2')
-    amount = request.form.get('value')
-    print(curr_one)
-    converted = c.convert(curr_one.upper(), curr_two.upper(), float(amount)) 
-    print(converted)
 
-    return render_template("converter.html", curr_one=curr_one, curr_two=curr_two, converted=converted)
+    if Currency_list.keys() >= {curr_one.upper(), curr_two.upper()}:
+
+        print('in if')
+        converted = c.convert(curr_one.upper(), curr_two.upper(), float(request.form.get('value'))) 
+        new_converted = float("{:.2f}".format(converted))
+        return render_template("converter.html", new_converted=new_converted)
+
+    else:
+
+        flash("Invalid currency")
+        return redirect("/")
+    
